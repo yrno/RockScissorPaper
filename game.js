e5d7e80f7188dec86e4dd74dc1485484da2ac0e8
Henry <henriksfurdal@outlook.com>
2021-04-23 13:19:16 +0200

    Initiated javascript (NOT FINISHED)

--- game.js
+++ game.js
@@ -1,4 +1,10 @@
 var userScore = 0;
 var systemScore = 0;
 
-function = buttonClick(buttonCli)
+var inputs = ["rock", "paper", "scissor"];
+
+function playGame (user){
+    Math.floor(Math.random())
+    console.log(inputs[Math.floor(Math.random()*3)]);
+    document.getElementById("Feedback").src = "images/"inputs[Math.floor(Math.random()*3)]".png"
+}   
