9e416b69e85c10541a0569b4bd70df0248ef80f4
Henry <henriksfurdal@outlook.com>
2021-04-23 14:54:03 +0200

    minor updates

--- game.js
+++ game.js
@@ -4,8 +4,16 @@ var systemScore = 0;
 var inputs = ["rock", "paper", "scissor"];
 
 function playGame (user){
+    var computer = inputs[Math.floor(Math.random()*3)]
     Math.floor(Math.random())
     console.log(inputs[Math.floor(Math.random()*3)]);
     document.getElementById("Feedback").src = "images/" + inputs[Math.floor(Math.random()*3)] + ".png";
-    console.log("images/" + inputs[Math.floor(Math.random()*3)] + ".png");
-}   
+    console.log("images/" + computer + ".png");
+
+    if (clicked == computer){
+        document.getElementById("").innerText = "uavgjort"
+    }
+    else
+    if(clicked == "scissor" && computer){
+
+    }
